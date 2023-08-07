49de82595e0ba33914ebd06aebc0934f21eef057
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2023-08-07 17:33:12 -0400

    Add Agent Armor

--- Assets/Scripts/Agent/AgentHealth.cs
+++ Assets/Scripts/Agent/AgentHealth.cs
@@ -5,28 +5,59 @@ using System;
 
 public class AgentHealth : MonoBehaviour
 {
-    public event Action OnDamageTaken;
+    public event Action OnArmorChange;
+    public event Action OnHealthChange;
     public event Action OnAgentDeath;
 
+    public float CurrentArmor => currentArmor;
     public float CurrentHealth => currentHealth;
+    public float MaxArmor => maxArmor;
     public float MaxHealth => maxHealth;
     public bool IsDead => isDead;
 
     bool isDead = false;
 
+    [SerializeField] bool allowArmorRegen = false;
+    [SerializeField] float armorRegenDelay = 2f;
+    [SerializeField] float armorRegenRate = 20f;
+    [SerializeField] float maxArmor = 100;
     [SerializeField] float maxHealth = 100f;
+    float currentArmor;
     float currentHealth;
+    float delayTimer;
 
     int hitSoundID;
 
     private void Awake()
     {
         currentHealth = maxHealth;
+        currentArmor = maxArmor;
     }
 
-    private void Start()
+    //private void Start()
+    //{
+    //    hitSoundID = SoundManager.Instance.GetSoundID("Agent_Hit");
+    //}
+
+    private void Update()
     {
-        hitSoundID = SoundManager.Instance.GetSoundID("Agent_Hit");
+        if (delayTimer == 0 && currentArmor < maxArmor)
+        {
+            currentArmor += armorRegenRate * Time.deltaTime;
+            if (currentArmor > maxArmor)
+            {
+                currentArmor = maxArmor;
+            }
+            OnArmorChange?.Invoke();
+        }
+        else
+        {
+            delayTimer -= Time.deltaTime;
+            if (delayTimer < 0)
+            {
+                delayTimer = 0;
+            }
+        }
     }
 
     public void Damage(float damage)
@@ -35,21 +66,35 @@ public class AgentHealth : MonoBehaviour
         {
             return;
         }
-        currentHealth -= damage;
-        //SoundManager.Instance.PlaySoundAtPosition(hitSoundID, transform.position);
-        if (currentHealth <= 0)
+        if (currentArmor >= damage)
         {
-            currentHealth = 0;
-            Die();
+            currentArmor -= damage;
+            delayTimer = armorRegenDelay;
+            OnArmorChange?.Invoke();
             return;
         }
-        OnDamageTaken?.Invoke();
+        else
+        {
+            damage -= currentArmor;
+            currentArmor = 0;
+            delayTimer = armorRegenDelay;
+            OnArmorChange?.Invoke();
+            currentHealth -= damage;
+            //SoundManager.Instance.PlaySoundAtPosition(hitSoundID, transform.position);
+            if (currentHealth <= 0)
+            {
+                currentHealth = 0;
+                Die();
+                return;
+            }
+            OnHealthChange?.Invoke();
+        }
     }
 
     void Die()
     {
         isDead = true;
-        OnDamageTaken?.Invoke();
+        OnHealthChange?.Invoke();
         OnAgentDeath?.Invoke();
     }
 }
--- Assets/Scripts/UI/HealthBarUI.cs
+++ Assets/Scripts/UI/HealthBarUI.cs
@@ -6,25 +6,30 @@ using UnityEngine.UI;
 public class HealthBarUI : MonoBehaviour
 {
     [SerializeField] Transform healthBarTransform;
+    [SerializeField] Transform armorBarTransform;
 
     AgentHealth agentHealth;
-    RawImage barImage;
-    Transform mainCam;
 
 
     private void Start()
     {
         agentHealth = FindObjectOfType<PlayerController>().GetComponent<AgentHealth>();
-        agentHealth.OnDamageTaken += UpdateHealthBar;
-        barImage = healthBarTransform.GetComponent<RawImage>();
-        mainCam = Camera.main.transform;
+        agentHealth.OnHealthChange += UpdateHealthBar;
+        agentHealth.OnArmorChange += UpdateArmorBar;
         UpdateHealthBar();
+        UpdateArmorBar();
     }
 
     private void UpdateHealthBar()
     {
         float percentHealth = agentHealth.CurrentHealth / agentHealth.MaxHealth;
         healthBarTransform.localScale = new Vector2(percentHealth, 1);
+    }
+
+    private void UpdateArmorBar()
+    {
+        float percentArmor = agentHealth.CurrentArmor / agentHealth.MaxArmor;
+        armorBarTransform.localScale = new Vector2(percentArmor, 1);
     }
 
 }
