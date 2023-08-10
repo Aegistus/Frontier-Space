937658cad1890d6703dfabd760900cc47a4bbde7
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2023-08-09 22:07:52 -0400

    Rework Heavy Door Sounds

--- Assets/Scripts/Level Objects/Door.cs
+++ Assets/Scripts/Level Objects/Door.cs
@@ -15,12 +15,17 @@ public class Door : MonoBehaviour
     [SerializeField] bool automatic = true;
     [SerializeField] bool locked = false;
     [SerializeField] string openSoundName = "Door_Open";
+    [SerializeField] string openEndSoundName = "Door_Open_End";
 
     public bool Open => open;
 
     bool transitioning = false;
     List<Collider> agentsWithinRange = new List<Collider>();
-    int soundID;
+    int openSoundID;
+    int openEndSoundID;
+    PositionalAudioSource doorOpenAudioSource;
+
+    readonly float positionalTolerance = .1f;
 
     private void Awake()
     {
@@ -40,7 +45,8 @@ public class Door : MonoBehaviour
 
     private void Start()
     {
-        soundID = SoundManager.Instance.GetSoundID(openSoundName);
+        openSoundID = SoundManager.Instance.GetSoundID(openSoundName);
+        openEndSoundID = SoundManager.Instance.GetSoundID(openEndSoundName);
     }
 
     private void Update()
@@ -50,17 +56,21 @@ public class Door : MonoBehaviour
             if (open)
             {
                 doorTransform.localPosition = Vector3.Lerp(doorTransform.localPosition, openPosition, doorMoveSpeed * Time.deltaTime);
-                if (Vector3.Distance(doorTransform.localPosition, openPosition) < .01f)
+                if (Vector3.Distance(doorTransform.localPosition, openPosition) < positionalTolerance)
                 {
                     transitioning = false;
+                    doorOpenAudioSource.Stop();
+                    SoundManager.Instance.PlaySoundAtPosition(openEndSoundID, transform.position);
                 }
             }
             else
             {
                 doorTransform.localPosition = Vector3.Lerp(doorTransform.localPosition, closedPosition, doorMoveSpeed * Time.deltaTime);
-                if (Vector3.Distance(doorTransform.localPosition, openPosition) < .01f)
+                if (Vector3.Distance(doorTransform.localPosition, openPosition) < positionalTolerance)
                 {
                     transitioning = false;
+                    doorOpenAudioSource.Stop();
+                    SoundManager.Instance.PlaySoundAtPosition(openEndSoundID, transform.position);
                 }
             }
         }
@@ -90,7 +100,7 @@ public class Door : MonoBehaviour
         {
             return;
         }
-        SoundManager.Instance.PlaySoundAtPosition(soundID, transform.position);
+        doorOpenAudioSource = SoundManager.Instance.PlaySoundAtPosition(openSoundID, transform.position);
         open = true;
         transitioning = true;
     }
