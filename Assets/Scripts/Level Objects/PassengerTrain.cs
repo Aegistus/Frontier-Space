c200d15405bb378da6caa928f9a1b134de5258ec
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2023-08-19 12:43:08 -0400

    Add Passenger Train Script and Button

---
+++ Assets/Scripts/Level Objects/PassengerTrain.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassengerTrain : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    [SerializeField] Vector3 destination;
+    [SerializeField] Door[] doors;
+
+    bool moving = false;
+
+    readonly float arrivalDistance = 1;
+
+    public void CallTrain()
+    {
+        moving = true;
+    }
+
+    public void OpenDoors()
+    {
+        for (int i = 0; i < doors.Length; i++)
+        {
+            doors[i].OpenDoor();
+        }
+    }
+
+    private void Update()
+    {
+        if (moving)
+        {
+            transform.position = Vector3.Lerp(transform.position, destination, moveSpeed * Time.deltaTime);
+        }
+        if (moving && Vector3.Distance(transform.position, destination) <= arrivalDistance)
+        {
+            moving = false;
+            OpenDoors();
+        }
+    }
+}
