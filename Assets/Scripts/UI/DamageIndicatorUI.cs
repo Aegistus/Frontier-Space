d7cf4baf7721739eeb8bff58fde9625f8e639445
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2023-10-08 20:34:15 -0400

    Add UI Hit Indicator When Player Damaged

---
+++ Assets/Scripts/UI/DamageIndicatorUI.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DamageIndicatorUI : MonoBehaviour
+{
+    [SerializeField] Image image;
+    [Range(0, 1)] [SerializeField] float transparencyIncreaseOnHit = .1f;
+    [SerializeField] float fadeSpeed = .1f;
+
+    AgentHealth playerHealth;
+
+    Color color;
+
+    private void Awake()
+    {
+        playerHealth = FindObjectOfType<PlayerController>().GetComponent<AgentHealth>();
+        playerHealth.OnDamageTaken += PlayerHealth_OnDamageTaken;
+        StartCoroutine(FadeIndicator());
+        color = image.color;
+        color.a = 0;
+        image.color = color;
+    }
+
+    private void PlayerHealth_OnDamageTaken(DamageSource obj)
+    {
+        print("TEST");
+        color = image.color;
+        color.a += transparencyIncreaseOnHit;
+        image.color = color;
+    }
+
+    IEnumerator FadeIndicator()
+    {
+        while (true)
+        {
+            yield return null;
+            color = image.color;
+            color.a -= fadeSpeed * Time.deltaTime;
+            image.color = color;
+        }
+    }
+}
