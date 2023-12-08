c49333064d975aa4d4debcd4f8e84016a1dbafec
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2023-12-08 17:01:09 -0500

    Add Weapon Kickback

--- Assets/Scripts/Weapons/RangedWeaponAttack.cs
+++ Assets/Scripts/Weapons/RangedWeaponAttack.cs
@@ -10,6 +10,9 @@ public abstract class RangedWeaponAttack : WeaponAttack
     [SerializeField] protected Transform weaponModel;
     [SerializeField] protected float recoilXRotation;
     [SerializeField] protected float recoilYRotation;
+    [SerializeField] protected float recoilKickback = .1f;
+    [SerializeField] protected float maxRecoilKickback = .1f;
+    [SerializeField] protected float kickbackRecovery = .05f;
     [SerializeField] protected float recoilRotationRecovery = 1f;
     [SerializeField] protected float aimFOVChange = 10f;
 
@@ -17,6 +20,8 @@ public abstract class RangedWeaponAttack : WeaponAttack
     public float AimFOVChange => aimFOVChange;
     protected WeaponAmmunition weaponAmmo;
     protected int shootSoundID;
+
+    float currentKickback;
 
     protected virtual void Awake()
     {
@@ -32,12 +37,15 @@ public abstract class RangedWeaponAttack : WeaponAttack
     {
         weaponModel.Rotate(Vector3.left, Random.Range(0, recoilXRotation));
         weaponModel.Rotate(Vector3.up, Random.Range(-recoilYRotation, recoilYRotation));
+        float kick = Mathf.Clamp(currentKickback + recoilKickback, 0, maxRecoilKickback);
+        transform.localPosition += Vector3.back * kick;
         OnRecoil.Invoke();
     }
 
     protected virtual void Update()
     {
         weaponModel.localRotation = Quaternion.Slerp(weaponModel.localRotation, Quaternion.identity, recoilRotationRecovery * Time.deltaTime);
+        weaponModel.localPosition = Vector3.Lerp(weaponModel.localPosition, Vector3.zero, kickbackRecovery * Time.deltaTime);
     }
 
 }
