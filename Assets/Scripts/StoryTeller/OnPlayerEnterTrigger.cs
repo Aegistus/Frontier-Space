2b7d1166445177d7e1d7a6f85fb7cb2cb070b199
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2024-02-03 22:06:02 -0500

    Level 03 Progress
    Add Tank Final Boss

--- Assets/Scripts/StoryTeller/OnPlayerEnterTrigger.cs
+++ Assets/Scripts/StoryTeller/OnPlayerEnterTrigger.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class StoryTrigger : MonoBehaviour
+public class OnPlayerEnterTrigger : MonoBehaviour
 {
     public UnityEvent OnTrigger;
 
