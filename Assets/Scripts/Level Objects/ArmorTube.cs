b5e3efbfcd8e7065a4c4a0de1966e6f363297ffc
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2024-04-18 19:39:47 -0400

    Level 01: Make Armor Tube One-Use Only

--- Assets/Scripts/Level Objects/ArmorTube.cs
+++ Assets/Scripts/Level Objects/ArmorTube.cs
@@ -6,11 +6,17 @@ using UnityEngine.Events;
 public class ArmorTube : MonoBehaviour, IInteractable
 {
     public string Description => "[E] Equip Armor";
-    public bool CurrentlyInteractable { get; } = true;
+    public bool CurrentlyInteractable { get; set; } = true;
     public UnityEvent OnEquip;
+
 
     public void Interact(GameObject _)
     {
+        if (!CurrentlyInteractable)
+        {
+            return;
+        }
         OnEquip.Invoke();
+        CurrentlyInteractable = false;
     }
 }
